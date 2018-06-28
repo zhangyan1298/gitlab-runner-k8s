017a9ef6be46fb0f338f8358b0ab30560b993c60
zhangyan1298 <38239794+zhangyan1298@users.noreply.github.com>
2018-06-28 17:29:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 gitlab-ci-multi-runner register -n --docker-privileged --kubernetes-privileged --run-untagged=true --locked=false --kubernetes-pull-policy=always
+exec gitlab-ci-multi-runner "$@"
